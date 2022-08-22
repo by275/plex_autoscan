d71bca43fd69b48d2e203d3e7510095be21ad55c
by275 <by275@protonmail.com>
2022-08-22 13:23:58 +0900

    deprecate response page using render_template
    * encourage to use CLI

--- autoscan/__main__.py
+++ autoscan/__main__.py
@@ -4,7 +4,7 @@ import sys
 import time
 from pathlib import Path
 
-from flask import Flask, abort, jsonify, request, render_template
+from flask import Flask, abort, jsonify, request
 
 # Get config
 from autoscan.config import Config
@@ -316,8 +316,8 @@ def client_pushed():
             )
             return f"Ignoring scan request because {ignore_match} was matched from your SERVER_IGNORE_LIST"
         if start_scan(final_path, "Manual", "Manual"):
-            return render_template("scan_ok.html", path=final_path)
-        return render_template("scan_not_ok.html", path=data["filepath"])
+            return jsonify({"success": True, "path": final_path})
+        return jsonify({"success": False, "path": data["filepath"]})
     elif data.get("eventType", "") == "Watcher" and data.get("pipe", ""):
         isfile, action, paths = utils.parse_watch_event(data["pipe"])
         if isfile and action in ("CREATE", "MOVE", "REMOVE"):
